8037f986031b354242bdd62d32de7c1870ce6bcd
Tomás Martins <tomas.campas.martins@gmail.com>
2025-04-18 12:47:34 +0100

    Initial version.
    Initial version of the pomodoro timer with start, pause and restart buttons.

---
+++ client/timer.js
@@ -0,0 +1,87 @@
+let targetDate;
+let timeLeft = 0;
+let timeLeftWhenPaused;
+let isItPaused = false;
+
+const startButton = document.getElementById('start');
+const restartButton = document.getElementById('restart');
+const pauseButton = document.getElementById('pause');
+
+
+startButton.addEventListener('click', () => {
+    unpauseCountdown();
+    showPauseButton();
+    if (timeLeft > 0 ) {
+        resumeCountdown();
+    }
+    else
+        startCountdown();
+  });
+
+restartButton.addEventListener('click', () => {
+    showPauseButton();
+    timeLeft = 0;
+    timeLeftWhenPaused = 0;
+
+    unpauseCountdown();
+    startCountdown();
+});
+
+pauseButton.addEventListener('click', () => {
+    showStartButton();
+    pauseCountdown();
+    const now = new Date().getTime();
+    timeLeftWhenPaused = targetDate - now;
+});
+
+function showPauseButton() {
+    pauseButton.style.display = "flex";
+    restartButton.style.display = "flex";
+    startButton.style.display = "none";
+}
+
+function showStartButton() {
+    startButton.style.display = "flex";
+    pauseButton.style.display = "none";
+    restartButton.style.display = "flex";
+}
+
+function pauseCountdown() {
+    isItPaused = true;
+}
+
+function unpauseCountdown() {
+    isItPaused = false;
+}
+
+function startCountdown() {
+    targetDate = new Date().getTime() + (1000 * 60 * 25);
+    updateCountdown();
+}
+
+function resumeCountdown() {
+    targetDate = new Date().getTime() + timeLeftWhenPaused;
+    updateCountdown();
+}
+
+// Countdown function to update the timer
+function updateCountdown() {
+    if (!isItPaused) {
+        const now = new Date().getTime();
+        timeLeft = targetDate - now;
+
+        // Calculate minutes, and seconds
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.max(0, Math.ceil((timeLeft % (1000 * 60)) / 1000));
+
+        // Display the result in the countdown div
+        document.getElementById("countdownText").innerHTML = minutes + ":" + seconds;
+
+        // Check if the countdown has ended
+        if (timeLeft > 0) {
+            setTimeout(updateCountdown, 1000); // Call updateCountdown again after 1 second
+        } else {
+            document.getElementById("countdown").innerHTML = "The event has started!";
+        }
+    }
+}
