8a2a7be4371b8e264df341eb1e43329d58918a94
Tomás Martins <tomas.campas.martins@gmail.com>
2025-05-15 15:47:27 +0100

    color schemes update.
    added color schemes to the scroll back to top button.

--- client/scrollToTopButton.js
+++ client/scrollToTopButton.js
@@ -3,7 +3,7 @@ const scrollToTopButton = document.getElementById("scrollToTopButton");
 
 // Show the button when the user scrolls down
 window.onscroll = function () {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+  if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
     scrollToTopButton.style.display = "flex"; // Show the button
   } else {
     scrollToTopButton.style.display = "none"; // Hide the button when at the top
